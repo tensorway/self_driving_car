3651318f2b6bf83bb8296d802579c8251bb690c2
tensorway <darijan.gudelj@gmail.com>
2021-09-23 00:42:10 +0200

    made controller class

--- actuation.py
+++ actuation.py
@@ -2,33 +2,69 @@
 import serial
 import time
 import subprocess
-#%%
+import logging
 
-# Open grbl serial port
-def get_device():
-    result = subprocess.check_output("ls -l /dev/ttyUSB*", shell=True, universal_newlines=True, stderr=subprocess.DEVNULL)
-    device = result.replace('\n', '').split(' ')[-1]
-    return device
+class Controller:
+
+    def __init__(self) -> None:
+        self.open()
+
+    def open(self):
+        self.serial = serial.Serial(self.get_device(), 115200)
+        # wakeup grbl
+        self.serial.write("\r\n\r\n".encode())
+        # Wait for grbl to initialize 
+        time.sleep(2)  
+
+    def get_device(self):
+        '''
+        get the serial port where 
+        something is connected
+        '''
+        result = subprocess.check_output("ls -l /dev/ttyUSB*", shell=True, universal_newlines=True, stderr=subprocess.DEVNULL)
+        device = result.replace('\n', '').split(' ')[-1]
+        return device
+
+    def move(self, x=None, y=None, speed=300, debug=False):
+        # Stream g-code to grbl
+        if x is None and y is None:
+            return
+        X = f'X{x}' if x is not None else ''
+        Y = f'Y{y}' if y is not None else ''
+        line = f"G1 {X} {Y} F{speed}" #XYgoto Fspeed
+        # line = "G92 X0 Y0" # zero
+        return self.send(line)
+
+    def home(self):
+        self.send("G28")
+
+    def zero(self):
+        self.send("G92 X0 Y0")
+
+    def send(self, line):
+        logging.debug('Sending: ' + line),
+        # Send g-code block to grbl
+        self.serial.write((line + '\r\n').encode()) 
+
+        # Wait for grbl response with carriage return
+        grbl_out = self.serial.readline().decode("utf-8").strip()
+        logging.info("grbl returned:"+ grbl_out)
+        logging.debug (' : ' + grbl_out)
+        return grbl_out
+
+    def close(self):
+        self.serial.close()
     
-s = serial.Serial(get_device(),115200)
 
-# time.sleep(2)   # Wait for grbl to initialize 
-# Wake up grbl
-s.write("\r\n\r\n".encode())
-time.sleep(2)   # Wait for grbl to initialize 
-s.flushInput()  # Flush startup text in serial input
-#%%
-# Stream g-code to grbl
-line = "G1 X2 Y3 F500"
-l = line.strip() # Strip all EOL characters for consistency
-print ('Sending: ' + l),
-s.write((l + '\n').encode()) # Send g-code block to grbl
-grbl_out = s.readline() # Wait for grbl response with carriage return
-print (' : ' + grbl_out.decode("utf-8").strip())
-#%%
-
-# Close file and serial port
-s.close()    
-#%%
 # %%
-get_device()
+if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s:%(levelname)s:%(message)s',
+        filename='example.log', 
+        encoding='utf8', 
+        level=logging.DEBUG)
+    controller = Controller()
+    controller.zero()
+    print(controller.move(15, 10))
+
+# %%
